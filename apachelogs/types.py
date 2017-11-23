ccb2675c7ef65ed62428d20e041329632d1326a5
John T. Wodder II <git@varonathe.org>
2017-11-23 23:19:59 +0000

    Support IPv6 addresses

--- apachelogs/types.py
+++ apachelogs/types.py
@@ -9,9 +9,22 @@ def clf(ftype):
         converter=lambda s: None if s == '-' else ftype.converter(s),
     )
 
-BYTE = r'(?:[1-9]?[0-9]|[1-9][0-9][0-9]|2[0-4][0-9]|25[0-5])'
-### TODO: Support IPv6:
-IP_ADDRESS_RGX = r'{BYTE}(?:\.{BYTE}){{3}}'.format(BYTE=BYTE)
+BYTE   = r'(?:[1-9]?[0-9]|[1-9][0-9][0-9]|2[0-4][0-9]|25[0-5])'
+HEXTET = r'[0-9A-Fa-f]{1,4}'
+IPv4   = r'{BYTE}(?:\.{BYTE}){{3}}'.format(BYTE=BYTE)
+
+IP_ADDRESS_RGX = r'''
+    {IPv4}
+    # Adapted from <https://git.io/vFxQW>:
+    |({HEXTET}:){{7}}({HEXTET}|:)
+    |({HEXTET}:){{6}}(:{HEXTET}|{IPv4}|:)
+    |({HEXTET}:){{5}}((:{HEXTET}){{1,2}}|:{IPv4}|:)
+    |({HEXTET}:){{4}}((:{HEXTET}){{1,3}}|(:{HEXTET})?:{IPv4}|:)
+    |({HEXTET}:){{3}}((:{HEXTET}){{1,4}}|(:{HEXTET}){{0,2}}:{IPv4}|:)
+    |({HEXTET}:){{2}}((:{HEXTET}){{1,5}}|(:{HEXTET}){{0,3}}:{IPv4}|:)
+    |({HEXTET}:){{1}}((:{HEXTET}){{1,6}}|(:{HEXTET}){{0,4}}:{IPv4}|:)
+    |:((:{HEXTET}){{1,7}}|(:{HEXTET}){{0,5}}:{IPv4}|:)
+'''.format(HEXTET=HEXTET, IPv4=IPv4)
 
 ip_address = FieldType(IP_ADDRESS_RGX, str)
 integer    = FieldType(r'(?:0|[1-9][0-9]*)', int)
