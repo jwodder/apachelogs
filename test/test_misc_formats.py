0df457c99138d6f5cc09486f531482db0409faae
John T. Wodder II <git@varonathe.org>
2018-02-16 00:25:18 +0000

    More better tests of `%{*}t` parsing

--- test/test_misc_formats.py
+++ test/test_misc_formats.py
@@ -1,4 +1,3 @@
-from   datetime   import datetime, timezone
 import pytest
 from   apachelogs import LogFormat
 
@@ -23,49 +22,6 @@ from   apachelogs import LogFormat
         '"GET /index.xml HTTP/1.1" "-"',
         {"request_line": "GET /index.xml HTTP/1.1"},
     ),
-
-    (
-        '%{%a %b %d}t %r',
-        'Sat Nov 25 GET / HTTP/1.1',
-        {
-            #"request_time_abbrev_wday": "Sat",
-            #"request_time_abbrev_mon": "Nov",
-            #"request_time_mday": 25,
-            "request_line": "GET / HTTP/1.1",
-        },
-    ),
-    (
-        '%{%A %B %d}t %r',
-        'Saturday November 25 GET / HTTP/1.1',
-        {
-            #"request_time_full_wday": "Saturday",
-            #"request_time_full_mon": "November",
-            #"request_time_mday": 25,
-            "request_line": "GET / HTTP/1.1",
-        },
-    ),
-    (
-        '%{%w (%u) %m/%d}t %r',
-        '6 (6) 11/25 GET / HTTP/1.1',
-        {
-            #"request_time_wday": 6,
-            #"request_time_iso_wday": 6,
-            #"request_time_mon": 11,
-            #"request_time_mday": 25,
-            "request_line": "GET / HTTP/1.1",
-        },
-    ),
-
-    (
-        '%{%s}t %r',
-        '1511642826 GET / HTTP/1.1',
-        {
-            #"request_time_unix": 1511642826,
-            "request_time": datetime(2017, 11, 25, 20, 47, 6, tzinfo=timezone.utc),
-            "request_line": "GET / HTTP/1.1",
-        },
-    ),
-
     (
         '%<s %s %>s',
         '201 202 203',
@@ -75,7 +31,6 @@ from   apachelogs import LogFormat
             "final_status": 203,
         },
     ),
-
 ])
 def test_parse_misc(fmt, entry, fields):
     assert dict(LogFormat(fmt, encoding='utf-8').parse(entry)) == fields
---
+++ test/test_parse_custom_time.py
@@ -0,0 +1,154 @@
+from   datetime   import datetime, timedelta, timezone
+import pytest
+from   apachelogs import LogFormat
+
+w5 = timezone(timedelta(hours=-5))
+
+@pytest.mark.parametrize('fmt,entry,fields,time_fields', [
+    (
+        '%{%a %b %d}t %r',
+        'Sat Nov 25 GET / HTTP/1.1',
+        {
+            "request_line": "GET / HTTP/1.1",
+            "request_time": None,
+        },
+        {
+            "abbrev_wday": "Sat",
+            "abbrev_mon": "Nov",
+            "mday": 25,
+        },
+    ),
+
+    (
+        '%{%A %B %d}t %r',
+        'Saturday November 25 GET / HTTP/1.1',
+        {
+            "request_line": "GET / HTTP/1.1",
+            "request_time": None,
+        },
+        {
+            "full_wday": "Saturday",
+            "full_mon": "November",
+            "mday": 25,
+        }
+    ),
+
+    (
+        '%{%w (%u) %m/%d}t %r',
+        '6 (6) 11/25 GET / HTTP/1.1',
+        {
+            "request_line": "GET / HTTP/1.1",
+            "request_time": None,
+        },
+        {
+            "wday": 6,
+            "iso_wday": 6,
+            "mon": 11,
+            "mday": 25,
+        }
+    ),
+
+    (
+        '%{%s}t %r',
+        '1511642826 GET / HTTP/1.1',
+        {
+            "request_line": "GET / HTTP/1.1",
+            "request_time": datetime(2017, 11, 25, 20, 47, 6, tzinfo=timezone.utc),
+        },
+        {"unix": 1511642826},
+    ),
+
+    (
+        '%{%s@%z}t %r',
+        '1511642826@-0500 GET / HTTP/1.1',
+        {
+            "request_line": "GET / HTTP/1.1",
+            "request_time": datetime(2017, 11, 25, 20, 47, 6, tzinfo=w5),
+        },
+        {"unix": 1511642826, "timezone": w5},
+    ),
+
+    (
+        '%{%Y-%m-%d %H:%M:%S}t %r',
+        '2017-11-25 20:47:06 GET / HTTP/1.1',
+        {
+            "request_line": "GET / HTTP/1.1",
+            "request_time": datetime(2017, 11, 25, 20, 47, 6),
+        },
+        {
+            "year": 2017,
+            "mon": 11,
+            "mday": 25,
+            "hour": 20,
+            "min": 47,
+            "sec": 6,
+        },
+    ),
+
+    (
+        '%{%Y-%m-%d %H:%M:%S %z}t %r',
+        '2017-11-25 20:47:06 -0500 GET / HTTP/1.1',
+        {
+            "request_line": "GET / HTTP/1.1",
+            "request_time": datetime(2017, 11, 25, 20, 47, 6, tzinfo=w5),
+        },
+        {
+            "year": 2017,
+            "mon": 11,
+            "mday": 25,
+            "hour": 20,
+            "min": 47,
+            "sec": 6,
+            "timezone": w5,
+        },
+    ),
+
+    (
+        '%{%s}t@%{%z}t %r',
+        '1511642826@-0500 GET / HTTP/1.1',
+        {
+            "request_line": "GET / HTTP/1.1",
+            "request_time": datetime(2017, 11, 25, 20, 47, 6, tzinfo=w5),
+        },
+        {"unix": 1511642826, "timezone": w5},
+    ),
+
+    (
+        '%{%Y-%m-%d}t %{%H:%M:%S}t %r',
+        '2017-11-25 20:47:06 GET / HTTP/1.1',
+        {
+            "request_line": "GET / HTTP/1.1",
+            "request_time": datetime(2017, 11, 25, 20, 47, 6),
+        },
+        {
+            "year": 2017,
+            "mon": 11,
+            "mday": 25,
+            "hour": 20,
+            "min": 47,
+            "sec": 6,
+        },
+    ),
+
+    (
+        '%{%Y-%m-%d}t %{%H:%M:%S}t %{%z}t %r',
+        '2017-11-25 20:47:06 -0500 GET / HTTP/1.1',
+        {
+            "request_line": "GET / HTTP/1.1",
+            "request_time": datetime(2017, 11, 25, 20, 47, 6, tzinfo=w5),
+        },
+        {
+            "year": 2017,
+            "mon": 11,
+            "mday": 25,
+            "hour": 20,
+            "min": 47,
+            "sec": 6,
+            "timezone": w5,
+        },
+    ),
+])
+def test_parse_custom_time(fmt, entry, fields, time_fields):
+    res = LogFormat(fmt, encoding='utf-8').parse(entry)
+    assert dict(res) == fields
+    assert res.time_fields == time_fields
