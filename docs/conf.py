8a636549fdf42701541bb7c41d9ee2d3b4e45ede
John T. Wodder II <git@varonathe.org>
2023-10-04 12:00:36 -0400

    Update docs/conf.py boilerplate

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,6 @@ copyright = "2017-2020 John Thorvald Wodder II"  # noqa: A001
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
-    "sphinx.ext.todo",
     "sphinx.ext.viewcode",
     "sphinx_copybutton",
 ]
@@ -23,14 +22,13 @@ intersphinx_mapping = {
 
 exclude_patterns = ["_build"]
 source_suffix = ".rst"
-source_encoding = "utf-8-sig"
+source_encoding = "utf-8"
 master_doc = "index"
 version = __version__
 release = __version__
 today_fmt = "%Y %b %d"
 default_role = "py:obj"
 pygments_style = "sphinx"
-todo_include_todos = True
 
 html_theme = "sphinx_rtd_theme"
 html_theme_options = {
